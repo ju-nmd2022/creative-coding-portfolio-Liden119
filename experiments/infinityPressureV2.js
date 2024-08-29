63ec02a18bacf5f8a2a1631c0f33972e87b21331
Liden119 <danielliden2@hotmail.com>
2024-08-29 16:54:07 +0200

    Color changes

--- experiments/infinityPressureV2.js
+++ experiments/infinityPressureV2.js
@@ -3,30 +3,29 @@ function setup(){
     frameRate(10);
 }
 
-const size = 100;
-const divider = 10;
-
-const colorValue = 255;
-const colorDivider = 50;
-let opacity = 150;
-
-
 function draw() {
     background(255, 255, 255);
+
+    const size = 100;
+    const divider = 10;
+
+    const colorValue = 255;
+    const colorDivider = 30;
+    opacity = 150;
+
     noStroke();
+
 
     for(let x = 100; x < 700; x += 100){
         const currentX = x;
         if (x===200 || x===400 || x===600){
             for(let y = 600; y > 50; y--){
 
-                //Color set
                 const rValue = noise(x/colorDivider, y/colorDivider) * colorValue;
                 const gValue = noise(x/colorDivider, y/colorDivider) * colorValue;
-                const bValue = 205 + (Math.random() * 50);
-                fill(rValue, gValue, bValue, opacity);
+                const bValue = noise(x/colorDivider, y/colorDivider) * colorValue;
 
-                //Elipse drawing
+                fill(rValue, gValue, bValue, opacity);
                 const value = noise(x/divider, y/divider) * size;
                 ellipse(x, y, value);
 
@@ -34,23 +33,12 @@ function draw() {
                     for(let x = currentX; x < currentX+100; x++){
                         const rValue = noise(x/colorDivider, y/colorDivider) * colorValue;
                         const gValue = noise(x/colorDivider, y/colorDivider) * colorValue;
-                        const bValue = 205 + (Math.random() * 50);
+                        const bValue = noise(x/colorDivider, y/colorDivider) * colorValue;
                 
                     fill(rValue, gValue, bValue, opacity);
                         const value = noise(x/divider, y/divider) * size;
                         ellipse(x, y, value);
                     }
-                    if(Math.random() > 0.5){
-                        opacity ++;
-                    } else{
-                        opacity--;
-                    }
-                }
-
-                if(Math.random() > 0.5){
-                    opacity ++;
-                } else{
-                    opacity--;
                 }
             }
 
@@ -58,18 +46,19 @@ function draw() {
         else {
             for(let y = 50; y < 600; y++){
 
-                const rValue = noise(x/colorDivider, y/colorDivider) * colorValue;
-                const gValue = noise(x/colorDivider, y/colorDivider) * colorValue;
-                const bValue = 205 + (Math.random() * 50);
-               fill(rValue, gValue, bValue, opacity);
+                const rValue = Math.random() * 100;
+                const gValue = Math.random() * 100;
+                const bValue = 150;
+                fill(rValue, gValue, bValue, opacity);
                 const value = noise(x/divider, y/divider) * size;
                 ellipse(x, y, value);
 
                 if (y === 599){
                     for(let x = currentX; x < currentX + 100; x++){
-                        const rValue = noise(x/colorDivider, y/colorDivider) * colorValue;
-                        const gValue = noise(x/colorDivider, y/colorDivider) * colorValue;
-                        const bValue = 205 + (Math.random() * 50);
+                        const rValue = Math.random() * 100;
+                        const gValue = Math.random() * 100;
+                        const bValue = 150;
+                
                 fill(rValue, gValue, bValue, opacity);
                         const value = noise(x/divider, y/divider) * size;
                         ellipse(x, y, value);
